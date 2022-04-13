5ec82cca983cb9ed184257232d81e4136c4d2ba5
changyeop.song <changyeop.song@wonderwall.kr>
2022-04-13 11:27:07 +0900

    install loadble

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Container, Main} from './styles';
 import {Route, Switch} from 'react-router';
+import loadable from '@loadable/component';
+
+import {Path} from 'Constants';
+
+const Home = loadable(() => import('_pages/Home'));
 
 function App() {
     return (
         <Container>
             <Main>
                 <Switch>
-                    <Route></Route>
+                    <Route component={Home} path={Path.Home} exact></Route>
                 </Switch>
             </Main>
         </Container>
